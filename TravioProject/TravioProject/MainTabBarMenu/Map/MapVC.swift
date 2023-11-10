d910d7988cfaea2e8550ca4413553220a2e42f20
BURAK600 <burakozer539@gmail.com>
2023-11-10 10:47:15 +0300

    MapVC selected places zoom rearranged

--- TravioProject/TravioProject/MainTabBarMenu/Map/MapVC.swift
+++ TravioProject/TravioProject/MainTabBarMenu/Map/MapVC.swift
@@ -211,15 +211,44 @@ func mapView(_ mapView: MKMapView, didDeselect view: MKAnnotationView) {
     
     func mapView(_ mapView: MKMapView, didSelect annotation: MKAnnotation) {
         
-        guard let selectedAnnotation = annotation as? PlaceAnnotation else { return }
+        
+        guard let selectedAnnotation = annotation as? PlaceAnnotation else {return}
+        
+//        if let annotation = view.annotation as? PlaceAnnotation {
+            
+        
+            let location = CLLocation(latitude: selectedAnnotation.coordinate.latitude, longitude: selectedAnnotation.coordinate.longitude)
+            let zoomRadius: CLLocationDistance = 400
+        
+            let region = MKCoordinateRegion(center: location.coordinate, latitudinalMeters: zoomRadius, longitudinalMeters: zoomRadius)
+        
+        let places = vm.places
+            let index = places.firstIndex(where: { $0.latitude == selectedAnnotation.coordinate.latitude && $0.longitude == selectedAnnotation.coordinate.longitude })
+        
+            let indexPath = IndexPath(item: index!, section: 0)
+                collectionView.scrollToItem(at: indexPath, at: .centeredHorizontally, animated: true)
+            mapView.setRegion(region, animated: true)
+            
+//            }
         
         
-        guard let selectedID = selectedAnnotation.placeId else {return}
-        let vc = PlaceDetailVC()
- 
+//        guard let selectedAnnotation = annotation as? PlaceAnnotation else {return}
+//        
+//        let places = vm.places
+//        places.enumerated().forEach({ placeIndex, place in
+//            if place.latitude == selectedAnnotation.coordinate.latitude && place.longitude == selectedAnnotation.coordinate.longitude {
+//              
+//                let temp = self.vm.places[0]
+//                self.vm.places[0] = self.vm.places[placeIndex]
+//                self.vm.places[placeIndex]  = temp
+//            }
+//        })
+//        reloadCollectionView()
+//        
+//        var selectedItemIndex = vm.getData?.data.places.filter({place in
+//           place.title == selectedAnnotation.titlePlace
+//        })
         
-        vc.selectedID = selectedID
-        self.navigationController?.pushViewController(vc, animated: true)
     }
 func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
         
@@ -262,10 +291,11 @@ extension MapVC:UICollectionViewDelegateFlowLayout {
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         
-        let selectedPlace = vm.places[indexPath.row]
-        let selectedCoordinate = CLLocationCoordinate2D(latitude: selectedPlace.latitude, longitude: selectedPlace.longitude)
-        let region = MKCoordinateRegion(center: selectedCoordinate, latitudinalMeters: 1000, longitudinalMeters: 1000)
-        mapView.setRegion(region, animated: true)
+        let selectedID = vm.places[indexPath.row].id
+        let vc = PlaceDetailVC()
+        vc.selectedID = selectedID
+        self.navigationController?.pushViewController(vc, animated: true)
+
     }
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         return CGSize(width: 310, height: 180 )
@@ -287,5 +317,20 @@ extension MapVC:UICollectionViewDataSource {
         cell.labelPlace.text = vm.places[indexPath.row].title
         return cell
     }
+    
+    
+    
+//    func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+//            if let annotation = view.annotation as? CustomAnnotation {
+//                let location = CLLocation(latitude: annotation.coordinate.latitude, longitude: annotation.coordinate.longitude)
+//                let zoomRadius: CLLocationDistance = 400
+//                let region = MKCoordinateRegion(center: location.coordinate, latitudinalMeters: zoomRadius, longitudinalMeters: zoomRadius)
+//                let index = mapAllPlaces.firstIndex(where: { $0.latitude == annotation.coordinate.latitude && $0.longitude == annotation.coordinate.longitude })
+//                let indexPath = IndexPath(item: index!, section: 0)
+//                    collectionView.scrollToItem(at: indexPath, at: .centeredHorizontally, animated: true)
+//                mapView.setRegion(region, animated: true)
+//                
+//                }
+//            }
 }
 
